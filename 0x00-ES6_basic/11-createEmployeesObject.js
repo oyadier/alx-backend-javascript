2584eb0278b4fa401bfb4dd7222794e27c57ea13
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-24 13:19:59 +0000

    Iterator

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,5 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
- let result = {}
- result[departmentName] = employees
+  const result = {};
+  result[departmentName] = employees;
   return result;
 }
