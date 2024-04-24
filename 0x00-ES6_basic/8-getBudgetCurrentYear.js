b1545ff825cce3d298ae888ea70b97fedbd55eb3
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-24 11:27:27 +0000

    ES6 method properties

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,6 +4,7 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
+  /*Calculated property name*/
   const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
