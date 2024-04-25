00c38e4d2c2096075e39f1c6bdbc75bbf3544e56
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-25 20:56:18 +0000

    First Commit

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+import ClassRoom  from "./0-classroom";
+
+export default function initializeRooms (){
+    const maxStudentsSizes = [19, 20, 34];
+
+    // Create an array of ClassRoom objects with different _maxStudentsSize values
+    const classrooms = maxStudentsSizes.map(size => new ClassRoom(size));
+
+    return classrooms;
+
+}
