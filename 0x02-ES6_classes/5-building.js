4116e318618d846c854f7539f02fe0e778ea6c0a
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-26 17:50:50 +0000

    A building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+class Building {
+  /**
+     *
+     * @param {Number} sqft square feet
+     */
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get squareFeet() {
+    return this._sqft;
+  }
+
+  set squareFeet(value) {
+    this._sqft = value;
+  }
+}
+export default Building;
