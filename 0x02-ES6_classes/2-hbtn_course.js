5a121f7dfbbca56c7c401e27df5e47a1b0215f92
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-26 15:18:32 +0000

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -8,19 +8,18 @@ class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name === 'string') {
       this._name = name;
-    }else {
-        throw new TypeError("Name must be a string")
+    } else {
+      throw new TypeError('Name must be a string');
     }
 
     if (typeof length === 'number') {
       this._length = length;
     } else {
-        throw new TypeError("Length must be a number")
+      throw new TypeError('Length must be a number');
     }
-    if (typeof students[0] === 'string'){
-         this._students = students;
-    } 
-   
+    if (typeof students[0] === 'string') {
+      this._students = students;
+    }
   }
   /**
  *
