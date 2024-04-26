9421cadf818288ac807c65b7d92c1a850ebcd08c
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-26 13:38:08 +0000

    Let make some classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,12 @@
-import ClassRoom from './0-classroom.js';
-
-
+import ClassRoom from './0-classroom';
+/**
+ * @returns group of three created Classrooms
+ */
 function initializeRoom() {
-return [
+  return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
-]
+    new ClassRoom(34),
+  ];
 }
-
 export default initializeRoom;
