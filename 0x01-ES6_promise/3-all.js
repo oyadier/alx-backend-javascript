479e410cae3e2475d9b156e385b2686ae1dfb1ea
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-27 09:51:27 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  return Promise.all([uploadPhoto, createUser]).then(
+  return Promise.all([uploadPhoto(), createUser()]).then(
     (resolve) => {
       console.log(`${resolve[0].body} ${resolve[1].firstName} ${resolve[1].lastName}`);
     },
