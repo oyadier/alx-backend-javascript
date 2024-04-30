75f076590404d97265568fb1c698d5e31da89aca
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-30 23:13:26 +0000

    combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,28 +1,13 @@
-/**
- * 
- * @param {Number} studentId 
- * @param {*Grade Objects} grades 
- * @returns 
- */
-const findGrade = (studentId, grades) => {
-  const grade = grades.find((grade) => grade.id === studentId);
-  return grade ? grade.grade : 'N/A';
-};
-/**
- *
- * @param {Array of obj} studentList
- * @param {*String } city location of students
- * @param {*Array} newGrade objects that contains grades by Id
- * @returns array of students contain grades
- */
-
-function updateStudentGradeByCity(studentList, city, newGrade) {
-  return studentList
-    .map((value) => ({
-      ...value,
-
-      grade: findGrade(studentList.id, newGrade),
-    }))
-    .filter((student) => student.location === city);
+function updateStudentGradeByCity(studentList, city, newGrades) {
+  if (studentList instanceof Array) {
+    return studentList.filter((student) => student.location === city)
+      .map((stu) => ({
+        id: stu.id,
+        firstName: stu.firstName,
+        location: stu.location,
+        grade: (newGrades.filter((grade) => grade.studentId === stu.id).pop() || { grade: 'N/A' }).grade,
+      }));
+  }
+  return [];
 }
 export default updateStudentGradeByCity;
