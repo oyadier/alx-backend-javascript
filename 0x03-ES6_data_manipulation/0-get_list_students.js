e5cc2b05ce44507c5b288801962357a4d0d2b4f5
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-30 04:18:42 +0000

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,19 +2,19 @@ function getListStudents() {
   return [
     {
       id: 1,
-      firstName: "Guillaume",
-      location: "San Francisco",
+      firstName: 'Guillaume',
+      location: 'San Francisco',
     },
     {
-        id: 2,
-        firstName: "James",
-        location: "Columbia",
-      },
-      {
-        id: 3,
-        firstName: "Serena",
-        location: "San Francisco",
-      },
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 3,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
   ];
 }
 export default getListStudents;
