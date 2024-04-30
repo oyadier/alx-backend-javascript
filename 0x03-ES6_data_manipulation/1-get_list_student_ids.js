dad7c2085242a91961aa2436dea476e59b3a4c2e
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-04-30 04:35:24 +0000

    More Mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {list} studentList
+ * @returns list of all ids from the students list
+ */
+function getListStudentIds(studentList) {
+    /**
+     * checks if the {@studentsList} is an array
+     */
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
+
+  return studentList.map((student) => student.id);
+}
+export default getListStudentIds;
