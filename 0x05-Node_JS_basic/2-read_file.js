ee6b39269240de4d607b5ec090646ab96bc47ace
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-08-21 00:32:54 +0100

    chore: Refactor countStudents function to use fs and csv-parser

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,30 +1,29 @@
 const { callbackify } = require('util');
+const fs = require('fs');
+const csv = require('csv-parser');
 
 function countStudents(dbpath) {
-
-    const fs = require('fs');
-    const csv = require('csv-parser');
-    const results = [];
-    fs.createReadStream(dbpath)
-        .pipe(csv())
-        .on('data', (data) => {
-            results.push(data);
-        })
-        .on('end', () => {
-            try {
-                const students = results
-                const CSstudents = students.filter((student) => student.field === 'CS');
-                const SWEstudents = students.filter((student) => student.field === 'SWE');
-                console.log(`Number of students: ${students.length - 1}`);
-                console.log(`Number of students in CS: ${CSstudents.length}. List: ${CSstudents.map((student) => student.firstname).join(', ')}`);
-                console.log(`Number of students in SWE: ${SWEstudents.length}. List: ${SWEstudents.map((student) => student.firstname).join(', ')}`);
-            } catch (error) {
-                throw new Error('Error processing the data:', error.message);
-            }
-        }).on('error', (error) => {
-            callbackify( new Error('Cannot load the database:' + error.message));
-        })
+  const results = [];
+  fs.createReadStream(dbpath)
+    .pipe(csv())
+    .on('data', (data) => {
+      results.push(data);
+    })
+    .on('end', () => {
+      try {
+        const students = results;
+        const CSstudents = students.filter((student) => student.field === 'CS');
+        const SWEstudents = students.filter((student) => student.field === 'SWE');
+        console.log(`Number of students: ${students.length - 1}`);
+        console.log(`Number of students in CS: ${CSstudents.length}. List: ${CSstudents.map((student) => student.firstname).join(', ')}`);
+        console.log(`Number of students in SWE: ${SWEstudents.length}. List: ${SWEstudents.map((student) => student.firstname).join(', ')}`);
+      } catch (error) {
+        throw new Error('Error processing the data:', error.message);
+      }
+    })
+    .on('error', (error) => {
+      callbackify(new Error(`Cannot load the database:${error.message}`));
+    });
 }
-
 
 module.exports = countStudents;
