872734cef110a16a771d3faaa98f1ab1975ce8e9
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-08-21 00:40:40 +0100

    Modify test file

--- 0x05-Node_JS_basic/test/test.js
+++ 0x05-Node_JS_basic/test/test.js
@@ -1,6 +1,6 @@
 
 // Date: 2021-06-13
-const displayMessage = require('./0-console');
+const displayMessage = require('../0-console');
 test('synchronous passing test', () => {
   const originalLog = console.log;
   console.log = jest.fn(); // Mock console.log
