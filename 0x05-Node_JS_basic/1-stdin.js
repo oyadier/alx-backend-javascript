76587c4e376babf65a00a14a9259837c83fcfaa2
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-08-21 00:24:43 +0100

    Using process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,9 @@
-const inp = process.stdin;
-console.log('Welcome to Holberton School, what is your name?');
-inp.setEncoding('utf-8');
-if (inp.isTTY) {
-  inp.on('data', (data) => {
-    console.log(`Your name is: ${data}`);
-    process.exit();
-  });
-} else {
-  inp.on('data', (data) => {
-    console.log(`Your name is: ${data}`);
-    console.log('This important software is now closing');
-    process.exit();
-  });
-}
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
