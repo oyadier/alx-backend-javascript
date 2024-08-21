08cecf917a20e2935468b0b1c752a4fdc15acf7f
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-08-21 03:33:21 +0100

    Modify Simple server: Nodejs

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,17 +1,24 @@
 const htp = require('http');
 
-const port = 1245;
-const host = '127.0.0.1';
-const app = htp.createServer((req, res) => {
+const PORT = 1245;
+const HOST = '127.0.0.1';
+
+const app = htp.createServer();
+app.on('request', (req, res) => {
+  const message = 'Hello Holberton Schoolss!';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('X-Powered-By', 'NodeJS');
+  res.setHeader('Content-Type', 'text/plain');
+  res.statusCode = 200;
+  res.sendDate = true;
 
   // HANDLING ALL ROUTES
-  res.end('Hello Holberton School!');
+  res.write(Buffer.from(message));
+  res.end();
 });
-
-app.listen(port, host, () => {
-  console.log(`Server running at http://${host}:${port}/`);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server running at http://${HOST}:${PORT}/`);
 });
 
 module.exports = app;
