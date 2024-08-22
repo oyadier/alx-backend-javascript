2c153d225da03e0e221a99f13ea6baa7f6546557
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-08-22 07:35:40 +0100

    First commit: all file

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,9 @@
+const calculateNumber = require("./0-calcul")
+var assert = require('assert')
+
+describe("addition using calculateNumber()", () => {
+    it("Two integers", () => assert.notEqual(calculateNumber(23, 1), 8))
+    it("Integer and floating point", () => assert.equal(calculateNumber(2, 3.3), 5.3))
+    it("Round float value", () => assert.equal(calculateNumber(4, 3.7), 7.7))
+    it ("Round floating value",() => assert.deepEqual(calculateNumber(2, 3.7), 5.7))
+})
