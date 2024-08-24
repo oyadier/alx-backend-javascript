c37c001e47a072597461203319d86d10915394cf
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-08-24 05:50:55 +0100

    Testing Express server

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -8,7 +8,7 @@ describe('Testing numbers', () => {
   it('2 is equal to 2', () => {
     expect(2 === 2).to.be.true;
   });
-
+//Skipping a failing test using .skip
   it.skip('1 is equal to 3', () => {
     expect(1 === 3).to.be.true;
   });
